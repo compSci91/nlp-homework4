6b24084a2355427a450bd97d32bda9e80666275f
JoshuaHowell <howjosh@gmail.com>
2018-11-13 23:31:16 -0600

    JH -- NEAR Operator -- Create a function for hits near Excellent.

--- code/near.py
+++ code/near.py
@@ -9,11 +9,21 @@ file_contents = file.read()
 
 file.close()
 
-phrase_regex = '(?:story_NN_I-NP\s._._B-O\sdjimon_JJ_B-NP)'
 
-word_regex = '(?:\s[^_]*_[^_]*_[^_\s]*){1,6}?'
-excellent_regex = '(?:\sexcellent_[^_]*_[^_\s]*)'
-matches = re.findall(r''+phrase_regex + word_regex +  excellent_regex, file_contents, re.M|re.I)
+def hitsNearExcellent(phrase, file_contents):
+    phrase = phrase.replace(' ', '\s')
+    phrase_regex = '(?:' + phrase + ')'
+    word_regex = '(?:\s[^_]*_[^_]*_[^_\s]*){1,6}?'
+    excellent_regex = '(?:\sexcellent_[^_]*_[^_\s]*)'
+    matches = re.findall(r''+phrase_regex + word_regex +  excellent_regex, file_contents, re.M|re.I)
+    return len(matches)
 
-for match in matches:
-    print match
+print hitsNearExcellent('story_NN_I-NP ._._B-O djimon_JJ_B-NP', file_contents)
+# phrase_regex = '(?:story_NN_I-NP\s._._B-O\sdjimon_JJ_B-NP)'
+
+# word_regex = '(?:\s[^_]*_[^_]*_[^_\s]*){1,6}?'
+# excellent_regex = '(?:\sexcellent_[^_]*_[^_\s]*)'
+# matches = re.findall(r''+phrase_regex + word_regex +  excellent_regex, file_contents, re.M|re.I)
+
+# for match in matches:
+#     print match
