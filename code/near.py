fa7e3ff6b7e5db547bfbd679f6927e2a140b4446
JoshuaHowell <howjosh@gmail.com>
2018-11-14 11:03:23 -0600

    JH -- NEAR Operator -- Complete NEAR Operator.

--- code/near.py
+++ code/near.py
@@ -2,7 +2,8 @@
 
 import re
 
-path = "/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework4/processed_docs/pos/cv017_22464.txt.out"
+# path = "/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework4/processed_docs/pos/cv017_22464.txt.out"
+path = "/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework4/processed_docs/neg/cv013_10494.txt.out"
 file  = open(path, 'r')
 
 file_contents = file.read()
@@ -15,8 +16,8 @@ def numberOfHitsPrior(phrase_regex, nearby_phrase, file_contents):
     word_regex = '(?:\s[^_]*_[^_]*_[^_\s]*){1,6}?'
     excellent_regex = '(?:\s'+ nearby_phrase + '_[^_]*_[^_\s]*)'
     matches = re.findall(r''+phrase_regex + word_regex +  excellent_regex, file_contents, re.M|re.I)
-    # for match in matches:
-    #     print match
+    for match in matches:
+        print match
     return len(matches)
 
 def numberOfHitsAfter(phrase_regex, nearby_phrase, file_contents):
@@ -34,4 +35,4 @@ def numberOfHitsNear(phrase, nearby_phrase, file_contents):
     return numberOfHitsPrior(phrase_regex, nearby_phrase, file_contents) + numberOfHitsAfter(phrase_regex, nearby_phrase, file_contents)
 
 
-print numberOfHitsNear('memorable_JJ_I-NP score_NN_I-NP ,_,_B-O', 'excellent', file_contents)
+print numberOfHitsNear('shao_JJ_B-NP khan_NN_I-NP was_VBD_B-VP', 'poor', file_contents)
