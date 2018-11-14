ffef675f0c552289c367becf448e0d7888ef02a8
JoshuaHowell <howjosh@gmail.com>
2018-11-14 15:37:53 -0600

    JH -- PhraseExtractor -- Extract phrase extraction into its own class.

--- code/main.py
+++ code/main.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python
 
-import re
+# from os import listdir
+# from os.path import isfile, join
+#
+# path = "/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework4/processed_docs/pos/"
+#
+# onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+#
+# print onlyfiles
+
+from phrase_retrieval import PhraseExtractor
 
 path = "/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework4/processed_docs/pos/cv007_4968.txt.out"
-file  = open(path, 'r')
 
-file_contents = file.read()
-
-file.close()
-
-
-
-#matches = re.findall(r'[\s]+[^_]*_JJ_[^_\s]*[\s]*[^_]*_(?:NN|NNS)_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
-#matches = re.findall(r'[\s]+[^_]*_(?:RB|RBR|RBS)_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^(?:NN|NNS)]*_[^_\s]*', file_contents, re.M|re.I)
-#matches = re.findall(r'[\s]+[^_]*_JJ_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
-matches = re.findall(r'[\s]+[^_]*_(?:NN|NNS)_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^(?:NN|NNS)]*_[^_\s]*', file_contents, re.M|re.I)
-#matches = re.findall(r'[\s]+[^_]*_(?:RB|RBR|RBS)_[^_\s]*[\s]*[^_]*_(?:VB|VBD|VBN|VBG)_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
-
-
-
-for match in matches:
-    print match
+PhraseExtractor(path)
---
+++ code/phrase_retrieval.py
@@ -0,0 +1,22 @@
+import re
+
+
+class PhraseExtractor:
+    def __init__(self, path):
+        path = "/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework4/processed_docs/pos/cv007_4968.txt.out"
+        file  = open(path, 'r')
+
+        file_contents = file.read()
+
+        file.close()
+
+        list1 = re.findall(r'[\s]+[^_]*_JJ_[^_\s]*[\s]*[^_]*_(?:NN|NNS)_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
+        list2 = re.findall(r'[\s]+[^_]*_(?:RB|RBR|RBS)_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^(?:NN|NNS)]*_[^_\s]*', file_contents, re.M|re.I)
+        list3 = re.findall(r'[\s]+[^_]*_JJ_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
+        list4 = re.findall(r'[\s]+[^_]*_(?:NN|NNS)_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^(?:NN|NNS)]*_[^_\s]*', file_contents, re.M|re.I)
+        list5 = re.findall(r'[\s]+[^_]*_(?:RB|RBR|RBS)_[^_\s]*[\s]*[^_]*_(?:VB|VBD|VBN|VBG)_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
+
+        phrases = list1 + list2 + list3 + list4 + list5
+
+        for phrase in phrases:
+            print phrase + '\n'
