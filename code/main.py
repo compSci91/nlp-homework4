e530c9623a4d6e08ecca8cd2d0e310b32ad35f73
JoshuaHowell <howjosh@gmail.com>
2018-11-13 22:17:46 -0600

    JH -- Regex -- Create final regexs.

--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@
 
 import re
 
-path = "/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework4/processed_docs/pos/cv000_29590.txt.out"
+path = "/Users/JoshuaHowell/Desktop/Texas A&M/Year 2/Fall 2018/Natural Language Processing/Homework4/processed_docs/pos/cv007_4968.txt.out"
 file  = open(path, 'r')
 
 file_contents = file.read()
@@ -13,7 +13,9 @@ file.close()
 
 #matches = re.findall(r'[\s]+[^_]*_JJ_[^_\s]*[\s]*[^_]*_(?:NN|NNS)_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
 #matches = re.findall(r'[\s]+[^_]*_(?:RB|RBR|RBS)_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^(?:NN|NNS)]*_[^_\s]*', file_contents, re.M|re.I)
-matches = re.findall(r'[\s]+[^_]*_JJ_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
+#matches = re.findall(r'[\s]+[^_]*_JJ_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
+#matches = re.findall(r'[\s]+[^_]*_(?:NN|NNS)_[^_\s]*[\s]*[^_]*_JJ_[^_\s]*[\s]*[^_]*_[^(?:NN|NNS)]*_[^_\s]*', file_contents, re.M|re.I)
+matches = re.findall(r'[\s]+[^_]*_(?:RB|RBR|RBS)_[^_\s]*[\s]*[^_]*_(?:VB|VBD|VBN|VBG)_[^_\s]*[\s]*[^_]*_[^_]*_[^_\s]*', file_contents, re.M|re.I)
 
 
 
