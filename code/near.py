56ef4dd82e713cedf9b0e5ca18e48cb5177ea6c6
JoshuaHowell <howjosh@gmail.com>
2018-11-14 08:59:39 -0600

    JH -- NEAR Operator -- Remove unnecessary comments.

--- code/near.py
+++ code/near.py
@@ -9,21 +9,16 @@ file_contents = file.read()
 
 file.close()
 
+##STILL NEED TO DO THE REVERSE
 
-def hitsNearExcellent(phrase, file_contents):
+def numberOfHitsNearExcellent(phrase, file_contents):
     phrase = phrase.replace(' ', '\s')
     phrase_regex = '(?:' + phrase + ')'
     word_regex = '(?:\s[^_]*_[^_]*_[^_\s]*){1,6}?'
     excellent_regex = '(?:\sexcellent_[^_]*_[^_\s]*)'
     matches = re.findall(r''+phrase_regex + word_regex +  excellent_regex, file_contents, re.M|re.I)
+    for match in matches:
+        print match
     return len(matches)
 
-print hitsNearExcellent('story_NN_I-NP ._._B-O djimon_JJ_B-NP', file_contents)
-# phrase_regex = '(?:story_NN_I-NP\s._._B-O\sdjimon_JJ_B-NP)'
-
-# word_regex = '(?:\s[^_]*_[^_]*_[^_\s]*){1,6}?'
-# excellent_regex = '(?:\sexcellent_[^_]*_[^_\s]*)'
-# matches = re.findall(r''+phrase_regex + word_regex +  excellent_regex, file_contents, re.M|re.I)
-
-# for match in matches:
-#     print match
+print numberOfHitsNearExcellent('story_NN_I-NP ._._B-O djimon_JJ_B-NP', file_contents)
